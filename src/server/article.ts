11a8bd0960f1e9fa1acab2943c834579371b4b17
方漓猫 <mucigames@outlook.com>
2024-07-06 20:41:06 +0800

    feat:添加了音乐控制相关脚本

--- src/server/article.ts
+++ src/server/article.ts
@@ -4,63 +4,73 @@ import { HyLetterTitle, HyLetterBody, HyStoryBody } from "../data/lang";
 /**
  * 注册书籍
  * @todo 书籍支持本地化
- * @todo 制作书籍收藏集
  */
 export function articleRegister() {
   const LETTER_1 = new article.Article(
     `hy:letter_1`,
     HyLetterTitle[1],
     HyLetterBody[1],
+    "textures/items/treasures/lost_letter"
   );
   const LETTER_2 = new article.Article(
     `hy:letter_2`,
     HyLetterTitle[2],
     HyLetterBody[2],
+    "textures/items/treasures/lost_letter"
   );
   const LETTER_3 = new article.Article(
     `hy:letter_3`,
     HyLetterTitle[3],
     HyLetterBody[3],
+    "textures/items/treasures/lost_letter"
   );
   const LETTER_4 = new article.Article(
     `hy:letter_4`,
     HyLetterTitle[4],
     HyLetterBody[4],
+    "textures/items/treasures/lost_letter"
   );
   const LETTER_5 = new article.Article(
     `hy:letter_5`,
     HyLetterTitle[5],
     HyLetterBody[5],
+    "textures/items/treasures/lost_letter"
   );
   const LETTER_6 = new article.Article(
     `hy:letter_6`,
     HyLetterTitle[6],
     HyLetterBody[6],
+    "textures/items/treasures/lost_letter"
   );
   const LETTER_7 = new article.Article(
     `hy:letter_7`,
     HyLetterTitle[7],
     HyLetterBody[7],
+    "textures/items/treasures/lost_letter"
   );
   const LETTER_8 = new article.Article(
     `hy:letter_8`,
     HyLetterTitle[8],
     HyLetterBody[8],
+    "textures/items/treasures/lost_letter"
   );
   const LETTER_9 = new article.Article(
     `hy:letter_9`,
     HyLetterTitle[9],
     HyLetterBody[9],
+    "textures/items/treasures/lost_letter"
   );
   const LETTER_10 = new article.Article(
     `hy:letter_10`,
     HyLetterTitle[10],
     HyLetterBody[10],
+    "textures/items/treasures/lost_letter"
   );
   const LETTER_11 = new article.Article(
     `hy:letter_11`,
     HyLetterTitle[11],
     HyLetterBody[11],
+    "textures/items/treasures/lost_letter"
   );
   const HIDDEN_STORIES = new article.ChapterArticle(
     "hy:story_book",
@@ -80,6 +90,7 @@ export function articleRegister() {
         body: HyStoryBody.section2,
       },
     ],
+    "textures/items/treasures/story_book"
   );
   const COLLECTION = new article.ArticleCollection(
     "hy:article_collection",
