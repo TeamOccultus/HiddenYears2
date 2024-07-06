96504507fe0131b93ef932ffe4e1703275fa021f
方漓猫 <mucigames@outlook.com>
2024-07-06 20:10:24 +0800

    feat:添加故事收藏集

--- src/server/article.ts
+++ src/server/article.ts
@@ -10,57 +10,57 @@ export function articleRegister() {
   const LETTER_1 = new article.Article(
     `hy:letter_1`,
     HyLetterTitle[1],
-    HyLetterBody[1]
+    HyLetterBody[1],
   );
   const LETTER_2 = new article.Article(
     `hy:letter_2`,
     HyLetterTitle[2],
-    HyLetterBody[2]
+    HyLetterBody[2],
   );
   const LETTER_3 = new article.Article(
     `hy:letter_3`,
     HyLetterTitle[3],
-    HyLetterBody[3]
+    HyLetterBody[3],
   );
   const LETTER_4 = new article.Article(
     `hy:letter_4`,
     HyLetterTitle[4],
-    HyLetterBody[4]
+    HyLetterBody[4],
   );
   const LETTER_5 = new article.Article(
     `hy:letter_5`,
     HyLetterTitle[5],
-    HyLetterBody[5]
+    HyLetterBody[5],
   );
   const LETTER_6 = new article.Article(
     `hy:letter_6`,
     HyLetterTitle[6],
-    HyLetterBody[6]
+    HyLetterBody[6],
   );
   const LETTER_7 = new article.Article(
     `hy:letter_7`,
     HyLetterTitle[7],
-    HyLetterBody[7]
+    HyLetterBody[7],
   );
   const LETTER_8 = new article.Article(
     `hy:letter_8`,
     HyLetterTitle[8],
-    HyLetterBody[8]
+    HyLetterBody[8],
   );
   const LETTER_9 = new article.Article(
     `hy:letter_9`,
     HyLetterTitle[9],
-    HyLetterBody[9]
+    HyLetterBody[9],
   );
   const LETTER_10 = new article.Article(
     `hy:letter_10`,
     HyLetterTitle[10],
-    HyLetterBody[10]
+    HyLetterBody[10],
   );
   const LETTER_11 = new article.Article(
     `hy:letter_11`,
     HyLetterTitle[11],
-    HyLetterBody[11]
+    HyLetterBody[11],
   );
   const HIDDEN_STORIES = new article.ChapterArticle(
     "hy:story_book",
@@ -79,6 +79,26 @@ export function articleRegister() {
         title: { translate: "hy.story.hs.title3" },
         body: HyStoryBody.section2,
       },
-    ]
+    ],
+  );
+  const COLLECTION = new article.ArticleCollection(
+    "hy:article_collection",
+    { translate: "hy.collection.title" },
+    { translate: "hy.collection.body" },
+    { commmand: "&collection" },
+    [
+      HIDDEN_STORIES,
+      LETTER_1,
+      LETTER_2,
+      LETTER_3,
+      LETTER_4,
+      LETTER_5,
+      LETTER_6,
+      LETTER_7,
+      LETTER_8,
+      LETTER_9,
+      LETTER_10,
+      LETTER_11,
+    ],
   );
 }
