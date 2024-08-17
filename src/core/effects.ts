49112cf1907c2534fc28f6ea5592be3831f9c83e
方琉璃 <mucigames@outlook.com>
2024-08-17 17:15:07 +0800

    fix:修复了流血Buff无法移除的漏洞

--- src/core/effects.ts
+++ src/core/effects.ts
@@ -1,4 +1,4 @@
-import { system } from "@minecraft/server";
+import { system, world } from "@minecraft/server";
 import { vanillaDimensions } from "lazuli-mc";
 
 /**
@@ -7,16 +7,42 @@ import { vanillaDimensions } from "lazuli-mc";
 export function bleedEffectMonitor() {
   vanillaDimensions.forEach((dimension) => {
     dimension.getEntities({ tags: ["hy:bleed_lv1"] }).forEach((entity) => {
-      system.runInterval(() => {
+      const num1 = system.runInterval(() => {
         entity.applyDamage(1);
         entity.addEffect("slowness", 40, { amplifier: 1 });
       }, 40);
+      system.runTimeout(()=>{
+        system.clearRun(num1);
+      },160)
+      world.afterEvents.entityDie.subscribe(event=>{
+        if(entity.id===event.deadEntity.id){
+          system.clearRun(num1);
+        }
+      })
+      world.afterEvents.entityRemove.subscribe(event=>{
+        if(entity.id===event.removedEntityId){
+          system.clearRun(num1);
+        }
+      })
     });
     dimension.getEntities({ tags: ["hy:bleed_lv2"] }).forEach((entity) => {
-      system.runInterval(() => {
+      const num2 = system.runInterval(() => {
         entity.applyDamage(1);
         entity.addEffect("slowness", 20, { amplifier: 1 });
       }, 20);
+      system.runTimeout(()=>{
+        system.clearRun(num2);
+      },160)
+      world.afterEvents.entityDie.subscribe(event=>{
+        if(entity.id===event.deadEntity.id){
+          system.clearRun(num2);
+        }
+      })
+      world.afterEvents.entityRemove.subscribe(event=>{
+        if(entity.id===event.removedEntityId){
+          system.clearRun(num2);
+        }
+      })
     });
   });
 }
--- src/core/itemSkills.ts
+++ src/core/itemSkills.ts
@@ -13,16 +13,16 @@ export function hammerSkill(entity: Entity, attacker?: Player) {
     entity.addEffect("mining_fatigue", 600);
     entity.addEffect("weakness", 300);
     attacker?.onScreenDisplay.setActionBar({
-      translate: "hy.itemSkill.hammer.1"
-    })
+      translate: "hy.itemSkill.hammer.1",
+    });
   } else if (CHANCE > 95) {
     entity.addEffect("mining_fatigue", 600);
     entity.addEffect("weakness", 300);
     entity.addEffect("darkness", 200);
     entity.applyDamage(2);
     attacker?.onScreenDisplay.setActionBar({
-      translate: "hy.itemSkill.hammer.2"
-    })
+      translate: "hy.itemSkill.hammer.2",
+    });
   }
 }
 
@@ -37,21 +37,21 @@ export function crowbarSkill(entity: Entity, attacker?: Player) {
   if (CHANCE < 50) {
     entity.applyDamage(3);
     attacker?.onScreenDisplay.setActionBar({
-      translate: "hy.itemSkill.crowbar.1"
-    })
+      translate: "hy.itemSkill.crowbar.1",
+    });
   } else if (CHANCE > 90) {
     entity.applyDamage(5);
     entity.addEffect("slowness", 400, { amplifier: 1 });
     attacker?.onScreenDisplay.setActionBar({
-      translate: "hy.itemSkill.crowbar.2"
-    })
+      translate: "hy.itemSkill.crowbar.2",
+    });
   }
 }
 
 /**
  * Special skill of awl.
- * @param entity 
- * @param attacker 
+ * @param entity
+ * @param attacker
  */
 export function awlSkill(entity: Entity, attacker?: Player) {
   const CHANCE: number = randomInteger(100);
@@ -60,22 +60,22 @@ export function awlSkill(entity: Entity, attacker?: Player) {
     entity.applyDamage(1);
     entity.addEffect("poison", 100);
     attacker?.onScreenDisplay.setActionBar({
-      translate: "hy.itemSkill.awl.1"
-    })
+      translate: "hy.itemSkill.awl.1",
+    });
   }
   if (CHANCE < 50) {
     entity.applyDamage(2);
     entity.addEffect("poison", 140);
     attacker?.onScreenDisplay.setActionBar({
-      translate: "hy.itemSkill.awl.2"
-    })
+      translate: "hy.itemSkill.awl.2",
+    });
   }
 }
 
 /**
  * Special skill of knife.
- * @param entity 
- * @param attacker 
+ * @param entity
+ * @param attacker
  */
 export function knifeSkill(entity: Entity, attacker?: Player) {
   const CHANCE: number = randomInteger(100);
@@ -83,26 +83,30 @@ export function knifeSkill(entity: Entity, attacker?: Player) {
   if (CHANCE < 50) {
     entity.addTag("hy:bleed_lv1");
     system.runTimeout(() => {
-      entity.removeTag("hy:bleed_lv1");
+      if (entity.isValid()) {
+        entity?.removeTag("hy:bleed_lv1");
+      }
     }, 160);
     attacker?.onScreenDisplay.setActionBar({
-      translate: "hy.itemSkill.knife.1"
-    })
+      translate: "hy.itemSkill.knife.1",
+    });
   } else if (CHANCE > 90) {
     entity.addTag("hy:bleed_lv2");
     system.runTimeout(() => {
-      entity.removeTag("hy:bleed_lv2");
+      if (entity.isValid()) {
+        entity?.removeTag("hy:bleed_lv2");
+      }
     }, 160);
     attacker?.onScreenDisplay.setActionBar({
-      translate: "hy.itemSkill.knife.2"
-    })
+      translate: "hy.itemSkill.knife.2",
+    });
   }
 }
 
 /**
  * Special skill of knife.
- * @param entity 
- * @param attacker 
+ * @param entity
+ * @param attacker
  */
 export function boardswordSkill(entity: Entity, attacker?: Player) {
   const CHANCE: number = randomInteger(100);
@@ -110,13 +114,13 @@ export function boardswordSkill(entity: Entity, attacker?: Player) {
   if (CHANCE < 20) {
     attacker?.addExperience(10);
     attacker?.onScreenDisplay.setActionBar({
-      translate: "hy.itemSkill.boardsword.1"
-    })
+      translate: "hy.itemSkill.boardsword.1",
+    });
   } else if (CHANCE > 95) {
     entity.applyDamage(2);
     attacker?.addLevels(1);
     attacker?.onScreenDisplay.setActionBar({
-      translate: "hy.itemSkill.boardsword.2"
-    })
+      translate: "hy.itemSkill.boardsword.2",
+    });
   }
 }
