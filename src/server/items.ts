a0bf4734708ccafcb404e1707cbb6c187f32c06e
方琉璃 <mucigames@outlook.com>
2024-08-17 13:43:27 +0800

    fix:修复了Failed to call function 'hasTag'的漏洞

--- src/server/items.ts
+++ src/server/items.ts
@@ -185,31 +185,31 @@ export class Item {
       /**
        * @tag hy:is_hammer-判断攻击物品是否为锤子
        */
-      if (ITEM.hasTag("hy:is_hammer")) {
+      if (ITEM?.hasTag("hy:is_hammer")) {
         hammerSkill(ENTITY, ATTACKER instanceof Player ? ATTACKER : undefined);
       }
       /**
        * @tag hy:is_corwbar-判断攻击物品是否为撬棍
        */
-      if (ITEM.hasTag("hy:is_crowbar")) {
+      if (ITEM?.hasTag("hy:is_crowbar")) {
         crowbarSkill(ENTITY, ATTACKER instanceof Player ? ATTACKER : undefined);
       }
       /**
        * @tag hy:is_awl-判断攻击物品是否为锥
        */
-      if (ITEM.hasTag("hy:is_awl")) {
+      if (ITEM?.hasTag("hy:is_awl")) {
         awlSkill(ENTITY, ATTACKER instanceof Player ? ATTACKER : undefined);
       }
       /**
        * @tag hy:is_knife-判断攻击物品是否为小刀
        */
-      if (ITEM.hasTag("hy:is_knife")) {
+      if (ITEM?.hasTag("hy:is_knife")) {
         knifeSkill(ENTITY, ATTACKER instanceof Player ? ATTACKER : undefined);
       }
       /**
        * @tag hy:magic_explode-判断物品是否可以进行法术爆发
        */
-      if (ITEM.hasTag("hy:magic_explode")) {
+      if (ITEM?.hasTag("hy:magic_explode")) {
         boardswordSkill(
           ENTITY,
           ATTACKER instanceof Player ? ATTACKER : undefined
@@ -218,7 +218,7 @@ export class Item {
       /**
        * @tag hy:corrosion_weapon-判断物品是否可以进行腐蚀攻击
        */
-      if (ITEM.hasTag("hy:corrosion_weapon")) {
+      if (ITEM?.hasTag("hy:corrosion_weapon")) {
         let num = system.runInterval(() => {
           ENTITY.applyDamage(2);
         }, 40);
