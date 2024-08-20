5d67e4ef8fb85fee118d41a6ab1447a18a4dec16
方琉璃 <mucigames@outlook.com>
2024-08-20 09:59:40 +0800

    feat:添加编译脚本

---
+++ build/script.mjs
@@ -0,0 +1,12 @@
+import * as esbuild from "esbuild";
+
+// Build script
+await esbuild.build({
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  outfile: "./HiddenYears_BP/scripts/index.js",
+  format: "esm",
+  external: ["@minecraft/server", "@minecraft/server-ui"],
+  minify: true,
+});
+console.info("script build done");
