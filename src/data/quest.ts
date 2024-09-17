00bf705a5227b39fbf72625c9ca8168d3f3acf99
方琉璃 <mucigames@outlook.com>
2024-09-17 10:56:03 +0800

    feat:为散·地底死城加入了任务引导

--- src/data/quest.ts
+++ src/data/quest.ts
@@ -1255,6 +1255,31 @@ export const LETTER_1 = new Quest(
   }
 );
 
+export const LETTER_2 = new Quest(
+  "letter_11",
+  { translate: "hy.quest.letter_2.title" },
+  { translate: "hy.quest.letter_2.body" },
+  {
+    condition: {
+      item: {
+        itemStack: new ItemStack("hy:letter_2"),
+        name: {
+          translate: "hy.item.lost_letter",
+        },
+      },
+    },
+    award: {
+      item: {
+        name: {
+          translate: "item.diamond.name",
+        },
+        itemStack: new ItemStack("diamond", 3),
+      },
+    },
+    iconPath: "textures/items/lost_letter",
+  }
+);
+
 export const LETTER_11 = new Quest(
   "letter_11",
   { translate: "hy.quest.readings3.title" },
--- src/server/registry/quest.ts
+++ src/server/registry/quest.ts
@@ -109,6 +109,7 @@ const LETTER_0 = new QuestBook(
       quests.STORY_BOOK,
       quests.LETTER_0,
       quests.LETTER_11,
+      quests.LETTER_2,
       quests.LETTER_1,
     ],
   }
