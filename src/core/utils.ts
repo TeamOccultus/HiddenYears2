cadf62e431af32bcfdd27526f4f411e03529c747
方琉璃 <mucigames@outlook.com>
2025-01-13 16:12:45 +0800

    feat:为其他阔剑实装了冷却

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,4 +1,9 @@
-import { affectEntities, damageEntities, getEquipmentItem, setEquipmentItem } from "@lazuli/ldk2";
+import {
+  affectEntities,
+  damageEntities,
+  getEquipmentItem,
+  setEquipmentItem,
+} from "@lazuli/ldk2";
 import {
   Dimension,
   Entity,
@@ -51,7 +56,7 @@ export class HyUtils {
   }
   static loot(dimension: Dimension, location: Vector3, path: string) {
     dimension.runCommand(
-      `loot spawn ${location.x} ${location.y} ${location.z} loot "${path}"`,
+      `loot spawn ${location.x} ${location.y} ${location.z} loot "${path}"`
     );
   }
   /**
@@ -141,6 +146,85 @@ export class HyUtils {
       };
       damageEntities(player.dimension, SKELETON_OPINION, 8);
       affectEntities(player.dimension, SKELETON_OPINION, "weakness", 300);
+    } else {
+      player.sendMessage([{ translate: "hy.message.no_exp" }]);
+    }
+  }
+  static flashMetalExplode(player: Player) {
+    if (player.level > 5) {
+      const ALL_OPTION: EntityQueryOptions = {
+        location: player.location,
+        maxDistance: 18,
+        excludeTags: ["hy.magic_explode"],
+        excludeFamilies: ["noaoe"],
+      };
+      damageEntities(player.dimension, ALL_OPTION, 8);
+      affectEntities(player.dimension, ALL_OPTION, "weakness", 300);
+    } else {
+      player.sendMessage([{ translate: "hy.message.no_exp" }]);
+    }
+  }
+  static corrosionExplode(player: Player) {
+    if (player.level > 5) {
+      const UNDEAD_OPINION: EntityQueryOptions = {
+        location: player.location,
+        maxDistance: 18,
+        families: ["undead"],
+      };
+      damageEntities(player.dimension, UNDEAD_OPINION, 8);
+      affectEntities(player.dimension, UNDEAD_OPINION, "weakness", 300);
+    } else {
+      player.sendMessage([{ translate: "hy.message.no_exp" }]);
+    }
+  }
+  static emeraldExplode(player: Player) {
+    if (player.level > 5) {
+      const ILLAGER_OPINION: EntityQueryOptions = {
+        location: player.location,
+        maxDistance: 18,
+        families: ["illager"],
+      };
+      damageEntities(player.dimension, ILLAGER_OPINION, 8);
+      affectEntities(player.dimension, ILLAGER_OPINION, "weakness", 300);
+    } else {
+      player.sendMessage([{ translate: "hy.message.no_exp" }]);
+    }
+  }
+  static flashCopperExplode(player: Player) {
+    if (player.level > 5) {
+      const ARTHROPOD_OPINION: EntityQueryOptions = {
+        location: player.location,
+        maxDistance: 18,
+        families: ["arthropod"],
+      };
+      damageEntities(player.dimension, ARTHROPOD_OPINION, 8);
+      affectEntities(player.dimension, ARTHROPOD_OPINION, "weakness", 300);
+    } else {
+      player.sendMessage([{ translate: "hy.message.no_exp" }]);
+    }
+  }
+  static amethystExplode(player: Player) {
+    if (player.level > 5) {
+      const POULTRY_OPINION: EntityQueryOptions = {
+        location: player.location,
+        maxDistance: 18,
+        families: ["poultry"],
+      };
+      damageEntities(player.dimension, POULTRY_OPINION, 8);
+      affectEntities(player.dimension, POULTRY_OPINION, "weakness", 300);
+    } else {
+      player.sendMessage([{ translate: "hy.message.no_exp" }]);
+    }
+  }
+  static rubyExplode(player: Player) {
+    if (player.level > 5) {
+      const RUBY_OPINION: EntityQueryOptions = {
+        location: player.location,
+        maxDistance: 18,
+        families: ["ruby"],
+      };
+      damageEntities(player.dimension, RUBY_OPINION, 8);
+      affectEntities(player.dimension, RUBY_OPINION, "weakness", 300);
     } else {
       player.sendMessage([{ translate: "hy.message.no_exp" }]);
     }
