82b17071288b793e2f5c28e59ff4e833f5630c30
方琉璃 <mucigames@outlook.com>
2025-02-28 09:12:43 +0800

    feat:更新文章系统

--- src/server/registry/article.ts
+++ src/server/registry/article.ts
@@ -1,8 +1,7 @@
 import { ItemStack, world } from "@minecraft/server";
 import {
-  ArticleBuilder,
-  ChapterArticleBuilder,
-  ArticleCollectionBuilder,
+  Article,
+  ArticleCenter,
 } from "@grindstone/article-kit";
 import {
   HyLetterBody,
@@ -11,121 +10,140 @@ import {
 } from "../../data/lang";
 import { giveItem } from "@grindstone/utils";
 
-const LETTER_1 = new ArticleBuilder(
+const LETTER_1 = new Article(
   `hy:letter_1`,
   { translate: "article.1.title" },
   HyLetterBody[1],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_2 = new ArticleBuilder(
+const LETTER_2 = new Article(
   `hy:letter_2`,
   { translate: "article.2.title" },
   HyLetterBody[2],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_3 = new ArticleBuilder(
+const LETTER_3 = new Article(
   `hy:letter_3`,
   { translate: "article.3.title" },
   HyLetterBody[3],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_4 = new ArticleBuilder(
+const LETTER_4 = new Article(
   `hy:letter_4`,
   { translate: "article.4.title" },
   HyLetterBody[4],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_5 = new ArticleBuilder(
+const LETTER_5 = new Article(
   `hy:letter_5`,
   { translate: "article.5.title" },
   HyLetterBody[5],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_6 = new ArticleBuilder(
+const LETTER_6 = new Article(
   `hy:letter_6`,
   { translate: "article.6.title" },
   HyLetterBody[6],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_7 = new ArticleBuilder(
+const LETTER_7 = new Article(
   `hy:letter_7`,
   { translate: "article.7.title" },
   HyLetterBody[7],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_8 = new ArticleBuilder(
+const LETTER_8 = new Article(
   `hy:letter_8`,
   { translate: "article.8.title" },
   HyLetterBody[8],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_9 = new ArticleBuilder(
+const LETTER_9 = new Article(
   `hy:letter_9`,
   { translate: "article.9.title" },
   HyLetterBody[9],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_10 = new ArticleBuilder(
+const LETTER_10 = new Article(
   `hy:letter_10`,
   { translate: "article.10.title" },
   HyLetterBody[10],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_11 = new ArticleBuilder(
+const LETTER_11 = new Article(
   `hy:letter_11`,
   { translate: "article.11.title" },
   HyLetterBody[11],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_12 = new ArticleBuilder(
+const LETTER_12 = new Article(
   `hy:letter_12`,
   { translate: "article.12.title" },
   HyLetterBody[12],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_13 = new ArticleBuilder(
+const LETTER_13 = new Article(
   `hy:letter_13`,
   { translate: "article.13.title" },
   HyLetterBody[13],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_14 = new ArticleBuilder(
+const LETTER_14 = new Article(
   `hy:letter_14`,
   { translate: "article.14.title" },
   HyLetterBody[14],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_15 = new ArticleBuilder(
+const LETTER_15 = new Article(
   `hy:letter_15`,
   { translate: "article.15.title" },
   HyLetterBody[15],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_16 = new ArticleBuilder(
+const LETTER_16 = new Article(
   `hy:letter_16`,
   { translate: "article.16.title" },
   HyLetterBody[16],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_17 = new ArticleBuilder(
+const LETTER_17 = new Article(
   `hy:letter_17`,
   { translate: "article.17.title" },
   HyLetterBody[17],
+  undefined,
   "textures/items/desert_mythology"
 );
-const LETTER_18 = new ArticleBuilder(
+const LETTER_18 = new Article(
   `hy:letter_18`,
   { translate: "article.18.title" },
   HyLetterBody[18],
+  undefined,
   "textures/items/lost_letter"
 );
-const LETTER_19 = new ArticleBuilder(
+const LETTER_19 = new Article(
   `hy:letter_19`,
   { translate: "article.19.title" },
   HyLetterBody[19],
+  undefined,
   "textures/items/lost_letter"
 );
-const HIDDEN_STORIES = new ChapterArticleBuilder(
+const HIDDEN_STORIES = new Article(
   "hy:story_book",
   { translate: "hy.item.story_book" },
   { translate: "hy.story.hs.body" },
@@ -145,35 +163,11 @@ const HIDDEN_STORIES = new ChapterArticleBuilder(
   ],
   "textures/items/story_book"
 );
-const COLLECTION = new ArticleCollectionBuilder(
+const COLLECTION = new ArticleCenter(
   "hy:article_collection",
   { translate: "hy.collection.title" },
   { translate: "hy.collection.body" },
-  {
-    itemStack: new ItemStack("hy:collection_book"),
-  },
-  [
-    HIDDEN_STORIES,
-    LETTER_1,
-    LETTER_2,
-    LETTER_3,
-    LETTER_4,
-    LETTER_5,
-    LETTER_6,
-    LETTER_7,
-    LETTER_8,
-    LETTER_9,
-    LETTER_10,
-    LETTER_11,
-    LETTER_12,
-    LETTER_13,
-    LETTER_14,
-    LETTER_15,
-    LETTER_16,
-    LETTER_17,
-    LETTER_18,
-    LETTER_19,
-  ]
+  true
 );
 
 export function registryArticle() {
@@ -183,25 +177,25 @@ export function registryArticle() {
       event.player.addTag("hy:get_collection");
     }
   });
-  HIDDEN_STORIES.build();
-  LETTER_1.build();
-  LETTER_2.build();
-  LETTER_3.build();
-  LETTER_4.build();
-  LETTER_5.build();
-  LETTER_6.build();
-  LETTER_7.build();
-  LETTER_8.build();
-  LETTER_9.build();
-  LETTER_10.build();
-  LETTER_11.build();
-  LETTER_12.build();
-  LETTER_13.build();
-  LETTER_14.build();
-  LETTER_15.build();
-  LETTER_16.build();
-  LETTER_17.build();
-  LETTER_18.build();
-  LETTER_19.build();
-  COLLECTION.build();
+  HIDDEN_STORIES.registryComponent("hy:hidden_story");
+  LETTER_1.registryComponent("hy:letter_1");
+  LETTER_2.registryComponent("hy:letter_2");
+  LETTER_3.registryComponent("hy:letter_3");
+  LETTER_4.registryComponent("hy:letter_4");
+  LETTER_5.registryComponent("hy:letter_5");
+  LETTER_6.registryComponent("hy:letter_6");
+  LETTER_7.registryComponent("hy:letter_7");
+  LETTER_8.registryComponent("hy:letter_8");
+  LETTER_9.registryComponent("hy:letter_9");
+  LETTER_10.registryComponent("hy:letter_10");
+  LETTER_11.registryComponent("hy:letter_11");
+  LETTER_12.registryComponent("hy:letter_12");
+  LETTER_13.registryComponent("hy:letter_13");
+  LETTER_14.registryComponent("hy:letter_14");
+  LETTER_15.registryComponent("hy:letter_15");
+  LETTER_16.registryComponent("hy:letter_16");
+  LETTER_17.registryComponent("hy:letter_17");
+  LETTER_18.registryComponent("hy:letter_18");
+  LETTER_19.registryComponent("hy:letter_19");
+  COLLECTION.registryComponent("hy:article_center");
 }
