67dd0b0b727c975aa3fa650b46366d056a84436d
方琉璃 <mucigames@outlook.com>
2025-03-22 10:49:56 +0800

    feat:重构了战利品表相关实现

---
+++ src/data/bundle.ts
@@ -0,0 +1,17 @@
+export const bundlesLoot = new Map();
+
+bundlesLoot
+  .set("minecraft:warden", "gameplay/bundles/warden")
+  .set("minecraft:witch", "gameplay/bundles/witch")
+  .set("minecraft:iron_golem", "gameplay/bundles/snow_golem")
+  .set("minecraft:ravager", "gameplay/bundles/illager")
+  .set("minecraft:pillager", "gameplay/bundles/illager")
+  .set("minecraft:vindicator_illager", "gameplay/bundles/illager")
+  .set("minecraft:evocation_illager", "gameplay/bundles/evocation_illager");
+
+export const bundlesBossLoot = new Map();
+
+bundlesBossLoot
+  .set("hy:mutas_wrath", "gameplay/bundles/mutas_wrath")
+  .set("hy:king_of_ruby", "gameplay/bundles/king_of_ruby")
+  .set("hy:pharaohs_ghost", "gameplay/bundles/pharaohs_ghost");
