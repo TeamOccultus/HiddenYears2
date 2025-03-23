0e9f957cfb84c32bdb707f6512b3af4c91e2988e
方琉璃 <mucigames@outlook.com>
2025-03-23 11:07:58 +0800

    chore:关闭了 ToolMaterial 的调试模式

--- src/server/registry/tool.ts
+++ src/server/registry/tool.ts
@@ -2,7 +2,7 @@ import { replaceLowerCopperTool } from "../../core/utils";
 import { ToolMaterial } from "@grindstone/material-kit";
 
 export function registryTool() {
-  const tool = new ToolMaterial("hy:custom_tools", true);
+  const tool = new ToolMaterial("hy:custom_tools");
   tool.onToolBreak((callback) => {
     replaceLowerCopperTool(callback.itemStack, callback.source);
   });
