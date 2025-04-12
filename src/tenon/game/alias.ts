c9be9e13f64d403635525dfc388f57b46d2631ea
方琉璃 <mucigames@outlook.com>
2025-04-12 18:41:23 +0800

    feat:加入了Alias的空壳

---
+++ src/tenon/game/alias.ts
@@ -0,0 +1,14 @@
+export class ItemAlias {
+  constructor(
+    public name: string,
+    public alias: string
+  ) {}
+}
+
+export class BlockAlias {
+    constructor(
+      public name: string,
+      public alias: string
+    ) {}
+  }
+  
