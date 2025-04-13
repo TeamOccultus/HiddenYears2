c25f50e55fd5d84b6d8a3c0258425208638821d1
方琉璃 <mucigames@outlook.com>
2025-04-13 16:45:29 +0800

    feat:加入了导引书的雏形

---
+++ src/data/encyclopedia.ts
@@ -0,0 +1,19 @@
+import { EncyclopediaEntry } from "../tenon/game/encyclopedia";
+
+export const welcome = new EncyclopediaEntry(
+  "hy:welcome",
+  { translate: "wiki.welcome.title" },
+  {
+    translate: "wiki.welcome.body",
+  },
+  "textures/items/clock"
+);
+
+export const magicPotion = new EncyclopediaEntry(
+    "hy:magic_potion",
+    { translate: "wiki.magic_potion.title" },
+    {
+      translate: "wiki.magic_potion.body",
+    },
+    "textures/items/medicine_1"
+  );
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { FoodRuntime } from './tenon/runtime/food'
+import { registryEncy } from "./server/registry/encyclopedia"
 
 
-new FoodRuntime("hy:effective_food")
+new FoodRuntime("hy:effective_food");
 
+registryEncy();
 console.log("Hello World!")
