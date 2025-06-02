11fe7db13278258ceab23b4134cee899febbf81c
方琉璃 <mucigames@outlook.com>
2025-06-02 18:32:43 +0800

    feat:允许第三方插件添加碎石机接口

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { registryNote } from "./server/registry/note";
 import { registryComponents } from "./server/registry/components";
 import { Crusher } from "./server/block/Crusher";
+import { CrusherRecipeManager } from "./server/recipe/CrusherRecipeManager";
 
 new Crusher();
+CrusherRecipeManager.openToPlugin();
 registryNote();
 registryComponents();
 console.log("Hello World!");
--- src/server/recipe/CrusherRecipeManager.ts
+++ src/server/recipe/CrusherRecipeManager.ts
@@ -1,4 +1,4 @@
-import { ItemStack } from "@minecraft/server";
+import { ItemStack, system } from "@minecraft/server";
 import { CrusherRecipe } from "./CrusherRecipeType";
 import { crusherIngredientData, crusherRecipesData } from "../../data/recipe/crusherRecipe";
 
@@ -16,6 +16,19 @@ export class CrusherRecipeManager {
   static addRecipe(recipe: CrusherRecipe) {
     this.recipes.push(recipe);
     this.ingredients.push(recipe.ingredient);
+  }
+  /**
+   * 允许第三方插件添加新的碎石机配方
+   * @example
+   * /scriptevent hiddenyears:addCrusherRecipe {"ingredient":"minecraft:brick_block","output":"minecraft:brick","amount":12}
+   */
+  static openToPlugin() {
+    system.afterEvents.scriptEventReceive.subscribe(arg=>{
+      if(arg.id!=="hiddenyears:addCrusherRecipe") return;
+      const data = JSON.parse(arg.message) as CrusherRecipe;
+      this.addRecipe(data);
+      console.log(`[隐藏之年] 已添加新的碎石机配方: ${data.ingredient} -> ${data.output} x${data.amount}`);
+    })
   }
 }
 
