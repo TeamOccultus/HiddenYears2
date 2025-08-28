c968e25422742feb8f6da92ce2a76949b660e460
方琉璃 <mucigames@outlook.com>
2025-08-28 16:39:14 +0800

    feat:加入了序章·第一幕任务

--- src/data/tasks.ts
+++ src/data/tasks.ts
@@ -1,42 +1,108 @@
-import { ItemStack } from "@minecraft/server";
-import { ItemAwards, ItemConditions, Task, TaskGroup } from "@occultus/api";
+import {
+  Format,
+  ItemAwards,
+  ItemConditions,
+  KillEntityFamilyConditions,
+  Task,
+  TaskGroup,
+  TaskServer,
+} from "@occultus/api";
 
-export const task1 = new Task("hiddenyears:task1", "任务1", "任务1的描述", {
-  awards: [new ItemAwards("hiddenyears:copper_coin", 10)],
-  conditions: [new ItemConditions("minecraft:stick")],
-  previousTask: "none",
-});
+export function getAllChapters() {
+  const server = new TaskServer();
 
-export const task2 = new Task("hiddenyears:task2", "任务2", "任务2的描述", {
-  awards: [new ItemAwards("hiddenyears:copper_coin", 20)],
-  conditions: [new ItemConditions("minecraft:diamond")],
-  previousTask: task1,
-});
+  const stick = new Task(
+    "hiddenyears:stick",
+    { translate: "task.stick" },
+    { translate: "task.stick.desc" },
+    {
+      awards: [new ItemAwards("hiddenyears:copper_coin", 3)],
+      conditions: [new ItemConditions("minecraft:stick")],
+      previousTask: "none",
+      iconPath: "textures/items/stick",
+    }
+  );
 
-export const task3 = new Task("hiddenyears:task3", "任务3", "任务3的描述", {
-  awards: [new ItemAwards("hiddenyears:copper_coin", 30)],
-  conditions: [new ItemConditions("minecraft:gold_ingot")],
-  previousTask: task2,
-});
+  const craftingTable = new Task(
+    "hiddenyears:crafting_table",
+    { translate: "task.crafting_table" },
+    { translate: "task.crafting_table.desc" },
+    {
+      awards: [new ItemAwards("hiddenyears:copper_coin", 4)],
+      conditions: [new ItemConditions("minecraft:crafting_table")],
+      previousTask: "none",
+      iconPath: "textures/ui/task/crafting_table",
+    }
+  );
 
-export const group1 = new TaskGroup(
-  "hiddenyears:group1",
-  "任务组1",
-  "任务组1的描述",
-  {
-    tasks: [task1, task2],
-    awards: [new ItemAwards("hiddenyears:copper_coin", 10)],
-    previousTask: "none",
-  }
-);
+  const furnace = new Task(
+    "hiddenyears:furnace",
+    { translate: "task.furnace" },
+    { translate: "task.furnace.desc" },
+    {
+      awards: [new ItemAwards("hiddenyears:copper_coin", 5)],
+      conditions: [new ItemConditions("minecraft:furnace")],
+      previousTask: "none",
+      iconPath: "textures/ui/task/furnace",
+    }
+  );
 
-export const group2 = new TaskGroup(
-  "hiddenyears:group2",
-  "任务组2",
-  "任务组2的描述",
-  {
-    tasks: [task3],
-    awards: [new ItemAwards("hiddenyears:copper_coin", 10)],
-    previousTask: group1,
-  }
-);
+  const bed = new Task(
+    "hiddenyears:bed",
+    { translate: "task.bed" },
+    { translate: "task.bed.desc" },
+    {
+      awards: [new ItemAwards("hiddenyears:copper_coin", 10)],
+      conditions: [new ItemConditions("minecraft:bed")],
+      previousTask: "none",
+      iconPath: "textures/items/bed_red",
+    }
+  );
+
+  const bread = new Task(
+    "hiddenyears:bread",
+    { translate: "task.bread" },
+    { translate: "task.bread.desc" },
+    {
+      awards: [new ItemAwards("minecraft:coal", 5)],
+      conditions: [new ItemConditions("minecraft:wheat")],
+      previousTask: "none",
+      iconPath: "textures/items/bread",
+    }
+  );
+
+  const monsterHunter = new Task(
+    "hiddenyears:monster_hunter",
+    { translate: "task.monster_hunter" },
+    { translate: "task.monster_hunter.desc" },
+    {
+      awards: [new ItemAwards("hiddenyears:gold_coin", 5)],
+      conditions: [
+        new KillEntityFamilyConditions("monster", "family.monster", this),
+      ],
+      previousTask: "none",
+      iconPath: "textures/items/iron_sword",
+    }
+  );
+  monsterHunter.pushToServer(server);
+
+  const prolugueActI = new TaskGroup(
+    "hiddenyears:prolugue_act_I",
+    { translate: "task.prolugue.act_1" },
+    {
+      rawtext: [
+        { translate: "task.prolugue.act_1.desc_1" },
+        { text: Format.newLine },
+        { translate: "task.prolugue.act_1.desc_2" },
+      ],
+    },
+    {
+      tasks: [stick, craftingTable, furnace, bed, bread, monsterHunter],
+      awards: [new ItemAwards("hiddenyears:copper_coin", 10)],
+      previousTask: "none",
+      iconPath: "textures/items/stick"
+    }
+  );
+
+  return [prolugueActI];
+}
--- src/server/registry/task.ts
+++ src/server/registry/task.ts
@@ -1,20 +1,21 @@
 import { TaskCenter } from "@occultus/api";
-import { group1, group2 } from "../../data/tasks";
+import { getAllChapters } from "../../data/tasks";
 import { CommandPermissionLevel } from "@minecraft/server";
 
 export const book = new TaskCenter(
     "hiddenyears:task_book",
-    "任务书",
-    "任务书的描述",
+    { translate: "task.title" },
+    { translate: "task.body" },
     {
-      tasks: [group1, group2],
+      tasks: getAllChapters(),
     }
   );
 
 export function registryTask() {
   book.addTrigger("hiddenyears:task_book", {
     name: "hiddenyears:task",
-    description: "打开任务书",
+    description: " %command.task.description",
     permissionLevel: CommandPermissionLevel.Any,
+    cheatsRequired: false
   });
 }
