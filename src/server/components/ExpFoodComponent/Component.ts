1100228a8d1af244ea41d736c984554ae5cb7d42
方琉璃 <mucigames@outlook.com>
2025-11-01 18:00:08 +0800

    fix:修复了ExpFoodComponent没有效果的漏洞

--- src/server/components/ExpFoodComponent/Component.ts
+++ src/server/components/ExpFoodComponent/Component.ts
@@ -1,12 +1,18 @@
-import { system } from "@minecraft/server";
-
+import { Player, system } from "@minecraft/server";
+import { ExpFoodComponentParams } from "./Params";
 
 export class ExpFoodComponent {
   constructor(readonly componentName: string) {
     system.beforeEvents.startup.subscribe((arg) => {
       arg.itemComponentRegistry.registerCustomComponent(this.componentName, {
         onConsume(arg0, arg1) {
-          
+          const { source, itemStack } = arg0;
+          if (!itemStack) return;
+          const p = arg1.params as ExpFoodComponentParams;
+          if(source instanceof Player){
+            source.addExperience(p.exp)
+            source.playSound(p.sound_event ?? "random.orb")
+          }
         },
       });
     });
--- src/server/components/ExpFoodComponent/Params.ts
+++ src/server/components/ExpFoodComponent/Params.ts
@@ -3,4 +3,8 @@ export type ExpFoodComponentParams = {
    * 食物给予的经验
    */
   exp: number;
+  /**
+   * 增加经验时发出的音效，默认为`random.orb`
+   */
+  sound_event?: string;
 };
