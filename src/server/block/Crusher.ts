d930fbe5d7abbe38d6ded59db1fbc420c3f612ac
方琉璃 <mucigames@outlook.com>
2025-11-15 13:11:33 +0800

    feat:为切割机加入了粒子效果

--- src/server/block/Crusher.ts
+++ src/server/block/Crusher.ts
@@ -1,19 +1,32 @@
-import { BlockEntity, BlockWithEntity, consumeAmount, setEquipmentItem, Vector3Utils } from "@occultus/api";
 import {
+  BlockEntity,
+  BlockWithEntity,
+  consumeAmount,
+  setEquipmentItem,
+  Vector3Utils,
+} from "@occultus/api";
+import {
+  DataDrivenEntityTriggerAfterEvent,
   ItemStack,
   PlayerInteractWithBlockAfterEvent,
+  world,
 } from "@minecraft/server";
 import { CrusherRecipeManager } from "../recipe/crusher/CrusherRecipeManager";
 
 export class Crusher extends BlockWithEntity {
   constructor() {
-    super("hiddenyears:crusher", "hiddenyears:crusher");
+    super(
+      "hiddenyears:crusher",
+      "hiddenyears:crusher",
+      "hiddenyears:crusher_tick"
+    );
   }
   onInteract(event: PlayerInteractWithBlockAfterEvent): void {
-    const [item, entityData, player] = [
+    const [item, entityData, player, block] = [
       event.beforeItemStack,
       this.getBlockEntityData(event.block),
       event.player,
+      event.block,
     ];
     if (!item) return;
     if (!entityData) return;
@@ -22,13 +35,16 @@ export class Crusher extends BlockWithEntity {
     console.warn(filledItem?.typeId);
     if (!filledItem) {
       if (!CrusherRecipeManager.ingredients.includes(item?.typeId)) {
-        player.sendMessage({ translate: "message.hiddenyears:cant_be_crushed" });
+        player.sendMessage({
+          translate: "message.hiddenyears:cant_be_crushed",
+        });
         return;
       }
       BlockEntity.storeItem(new ItemStack(item.typeId), entityData);
       const eqItem = consumeAmount(item, 1);
       setEquipmentItem(player, eqItem);
       player.playSound("fall.stone");
+      console.log("Particle spawned at ", entityData.entity.location);
       return;
     }
     if (item?.hasTag("minecraft:is_pickaxe")) {
@@ -41,5 +57,18 @@ export class Crusher extends BlockWithEntity {
       player.playSound("random.anvil_use");
       BlockEntity.clearStoredItem(entityData);
     }
+  }
+  onTick(args: DataDrivenEntityTriggerAfterEvent): void {
+    const entityData = BlockEntity.getData(args.entity);
+    if (!entityData) return;
+    const filledItem = BlockEntity.getStoredItem(entityData);
+    if (!filledItem) return;
+    if (Array.isArray(filledItem)) return;
+    const { x, y, z } = entityData.block.location;
+    entityData.entity.dimension.spawnParticle(filledItem.typeId, {
+      x: x,
+      y: y + 1,
+      z: z,
+    });
   }
 }
