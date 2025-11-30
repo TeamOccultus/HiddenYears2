69db605ef1f510a9f60210e8f43dea9529d8203c
方琉璃 <mucigames@outlook.com>
2025-11-30 12:05:42 +0800

    feat:改进了第三章通关字幕的表现

--- src/data/monologues.ts
+++ src/data/monologues.ts
@@ -1,7 +1,14 @@
-import { ItemStack, Player, system } from "@minecraft/server";
+import {
+  HudElement,
+  HudVisibility,
+  ItemStack,
+  Player,
+  system,
+} from "@minecraft/server";
 import { giveItem } from "@occultus/api";
 
 export function listenIsisMonologue(player: Player) {
+  player.onScreenDisplay.hideAllExcept([]);
   player.addEffect("regeneration", 120, { amplifier: 5, showParticles: false });
   player.camera.fade({
     fadeTime: {
@@ -11,45 +18,48 @@ export function listenIsisMonologue(player: Player) {
     },
   });
   player.sendMessage({
-    translate: "monologue.hiddenyears:.mutas_wrath_dead.1",
+    translate: "monologue.hiddenyears:mutas_wrath_dead.1",
   });
   system.runTimeout(() => {
     player.sendMessage({
-      translate: "monologue.hiddenyears:.mutas_wrath_dead.2",
+      translate: "monologue.hiddenyears:mutas_wrath_dead.2",
     });
   }, 40);
   system.runTimeout(() => {
     player.sendMessage({
-      translate: "monologue.hiddenyears:.mutas_wrath_dead.3",
+      translate: "monologue.hiddenyears:mutas_wrath_dead.3",
     });
   }, 80);
   system.runTimeout(() => {
     player.sendMessage({
-      translate: "monologue.hiddenyears:.mutas_wrath_dead.4",
+      translate: "monologue.hiddenyears:mutas_wrath_dead.4",
     });
   }, 120);
   system.runTimeout(() => {
     player.sendMessage({
-      translate: "monologue.hiddenyears:.mutas_wrath_dead.5",
+      translate: "monologue.hiddenyears:mutas_wrath_dead.5",
     });
   }, 160);
   system.runTimeout(() => {
     player.sendMessage({
-      translate: "monologue.hiddenyears:.mutas_wrath_dead.6",
+      translate: "monologue.hiddenyears:mutas_wrath_dead.6",
     });
   }, 200);
   system.runTimeout(() => {
-    giveItem(player, new ItemStack("hy:letter_19"));
+    // giveItem(player, new ItemStack("hiddenyears:letter_19"));
     player.sendMessage({
-      translate: "monologue.hiddenyears:.mutas_wrath_dead.7",
+      translate: "monologue.hiddenyears:mutas_wrath_dead.7",
       with: [player.name],
     });
     player.playSound("ramdom.levelup");
-    player.onScreenDisplay.setTitle({ translate: "hy.title.desert_book" });
+    player.onScreenDisplay.setTitle({
+      translate: "title.hiddenyears:desert_book",
+    });
     player.onScreenDisplay.updateSubtitle({
-      translate: "hy.title.desert_book.subtitle",
+      translate: "title.hiddenyears:desert_book.subtitle",
     });
   }, 240);
-  player.setDynamicProperty("hy:has_listened_isis_monologue", true);
+  player.setDynamicProperty("hiddenyears:has_listened_isis_monologue", true);
+  player.onScreenDisplay.resetHudElementsVisibility();
   // ISIS_CROWN.complete(player);
 }
