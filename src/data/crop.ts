c21f4b705c51a74c20b59778e3ec6b10cd2dbc0a
方琉璃 <mucigames@outlook.com>
2026-01-24 10:38:14 +0800

    chore:拆分农夫/牧者草帽的数据文件

--- src/data/crop.ts
+++
@@ -1,8 +0,0 @@
-const crop = new Map<string, string>();
-
-crop.set("minecraft:wheat", "minecraft:wheat");
-crop.set("minecraft:beetroot", "minecraft:beetroot");
-crop.set("minecraft:carrots", "minecraft:carrot");
-crop.set("minecraft:potatoes", "minecraft:potato");
-
-export default crop;
--- src/data/herd.ts
+++
@@ -1,9 +0,0 @@
-const herd = new Map<string, string>();
-
-herd.set("minecraft:pig", "minecraft:porkchop");
-herd.set("minecraft:cow", "minecraft:beef");
-herd.set("minecraft:chicken", "minecraft:chicken");
-herd.set("minecraft:sheep", "minecraft:mutton");
-herd.set("minecraft:rabbit", "minecraft:rabbit");
-
-export default herd;
