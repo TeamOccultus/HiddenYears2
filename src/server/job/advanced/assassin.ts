26b48ee6f97ea9b8c4a9755bcbf41fd86cdab497
方琉璃 <mucigames@outlook.com>
2026-02-06 15:53:34 +0800

    feat:加入了刺客的主动技能

--- src/server/job/advanced/assassin.ts
+++ src/server/job/advanced/assassin.ts
@@ -1,9 +1,10 @@
-import { Player } from "@minecraft/server";
+import { EntityDamageCause, Player, TicksPerSecond } from "@minecraft/server";
 import {
+  EntitiesUtils,
   getEquipmentItem,
   ItemConditions,
   Job,
-  RandomEvent
+  JobSkill
 } from "@occultus/api";
 
 export const assassin = new Job(
@@ -22,16 +23,64 @@ export const assassin = new Job(
   }
 );
 
-// 这里不能用hit entity之后等待hurt事件来实现，MS的时序炸的干干净净，全都是race condition
-// 我草泥马Mojang到底是怎么想的
+const skill1 = new JobSkill(
+  "hiddenyears:assassin_skill_1",
+  "hiddenyears:job_skill",
+  {
+    translate: "skill.assassin.1"
+  },
+  {
+    text: "？？？"
+  },
+  5 * TicksPerSecond
+);
+
+const skill2 = new JobSkill(
+  "hiddenyears:assassin_skill_2",
+  "hiddenyears:job_skill",
+  {
+    translate: "skill.assassin.2"
+  },
+  {
+    text: "？？？"
+  },
+  5 * TicksPerSecond
+);
+
+skill1.onRelease((arg) => {
+  arg.source.addEffect("minecraft:invisibility", 10 * TicksPerSecond, {
+    amplifier: 0,
+    showParticles: false
+  });
+});
+
+skill2.onRelease((arg) => {
+  arg.source.addEffect("minecraft:invisibility", 12 * TicksPerSecond, {
+    amplifier: 0,
+    showParticles: false
+  });
+  // 下次攻击必定暴击，这里不用自带的标签判断是因为这个效果无视技能持续时间
+  arg.source.addTag("hiddenyears:critical_next_attack");
+  arg.source.onScreenDisplay.setActionBar({
+    translate: "message.hiddenyears:critical_next_attack"
+  });
+});
+
+assassin.config.skills = [skill1, skill2];
+
 assassin.onCauseDamage((arg) => {
   const player = arg.damageSource.damagingEntity as Player;
-  const mainHandItem = getEquipmentItem(player);
-  // 要求主手得是匕首
-  // 副手拿不了匕首，不用判断
-  if (!mainHandItem?.hasTag("hiddenyears:is_dagger")) return;
   const hurtEntity = arg.hurtEntity;
-  if (!hurtEntity.isValid) return;
-  // 额外提升50%等级的伤害
-  hurtEntity.applyDamage(arg.damage * assassin.getLevel(player) * 0.8);
+  const mainHandItem = getEquipmentItem(player);
+  if (player.hasTag("hiddenyears:critical_next_attack")) {
+    if (hurtEntity.isValid)
+      hurtEntity.applyDamage(arg.damage * assassin.getLevel(player) * 1.8);
+    if (hurtEntity.isValid)
+      hurtEntity.addEffect("minecraft:potion", 5 * TicksPerSecond);
+  }
+  if (mainHandItem?.hasTag("hiddenyears:is_dagger")) {
+    if (!hurtEntity.isValid) return;
+    // 额外提升50%等级的伤害
+    hurtEntity.applyDamage(arg.damage * assassin.getLevel(player) * 0.8);
+  }
 });
