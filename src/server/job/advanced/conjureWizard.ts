96f64e281bb912e86969ceef11607fe6c46af4b5
方琉璃 <mucigames@outlook.com>
2026-02-06 17:01:14 +0800

    feat:加入了唤灵法师的技能
    btw, 不要问我为什么召唤出来的实体叫gun_mu（棍母），因为它不存在于游戏中 :)

--- src/server/job/advanced/conjureWizard.ts
+++ src/server/job/advanced/conjureWizard.ts
@@ -1,4 +1,12 @@
-import { ItemConditions, Job } from "@occultus/api";
+import { TicksPerSecond, EntityDamageCause, system } from "@minecraft/server";
+import {
+  EntitiesUtils,
+  ItemConditions,
+  Job,
+  JobSkill,
+  toVec3,
+  Vector3Utils
+} from "@occultus/api";
 
 export const conjureWizard = new Job(
   "hiddenyears:conjure_wizard",
@@ -15,3 +23,49 @@ export const conjureWizard = new Job(
     ]
   }
 );
+const skill1 = new JobSkill(
+  "hiddenyears:conjure_wizard_skill_1",
+  "hiddenyears:job_skill",
+  {
+    translate: "skill.conjure_wizard.1"
+  },
+  {
+    text: "？？？"
+  },
+  0
+);
+
+const skill2 = new JobSkill(
+  "hiddenyears:conjure_wizard_skill_2",
+  "hiddenyears:job_skill",
+  {
+    translate: "skill.conjure_wizard.2"
+  },
+  {
+    text: "？？？"
+  },
+  0
+);
+
+skill1.onRelease((arg) => {
+  const player = arg.source;
+  // TODO: 制作仆从
+  const entity = player.dimension.spawnEntity(
+    "hiddenyears:gun_mu",
+    Vector3Utils.add(player.location, toVec3(0, 1, 0))
+  );
+  system.runTimeout(() => {
+    if (entity.isValid) entity?.kill();
+  }, 8 * TicksPerSecond);
+});
+
+// 引爆范围内所有己方灵体，对周围 5 格敌人造成等级 * 5 的魔法伤害。
+skill2.onRelease((arg) => {
+  const player = arg.source;
+  const utils = new EntitiesUtils(player.dimension, {
+    location: player.location,
+    maxDistance: 5,
+    families: ["monster"]
+  });
+  utils.applyDamage(5, { cause: EntityDamageCause.magic });
+});
