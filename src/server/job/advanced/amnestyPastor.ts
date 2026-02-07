36b5426761809a63c2168443b4c25cd9ec5341c2
RawDiamondMC <RawDiamondMC@outlook.com>
2026-02-07 22:22:42 +0800

    Revert "fix: 修复并修改了一部分职业的实现"
    This reverts commit dd53c241f038d810d2f9b31b71661a587689b5ed.

--- src/server/job/advanced/amnestyPastor.ts
+++ src/server/job/advanced/amnestyPastor.ts
@@ -53,10 +53,10 @@ const skill2 = new JobSkill(
   10 * TicksPerSecond
 );
 
+skill1.onRelease((arg) => {});
+
 skill2.onRelease((arg) => {
   const player = arg.source;
-  // 圣罚赎免：领域持续期间为范围内友方（含自身）提供伤害吸收 III 30秒。
-  // TODO: 实际实现为释放时单次提供效果，与设定不符
   new EntitiesUtils(player.dimension, {
     location: player.location,
     maxDistance: 8,
@@ -66,12 +66,7 @@ skill2.onRelease((arg) => {
     duration: 30 * TicksPerSecond,
     amplifier: 2
   });
-  // 圣罚赎免：领域持续期间，对自身周围 8 格内所有敌方每秒等级*0.2的额外伤害
   const handle = system.runInterval(() => {
-    if (!skill2.isReleasing(player)) {
-      system.clearRun(handle);
-      return;
-    }
     new EntitiesUtils(player.dimension, {
       location: player.location,
       maxDistance: 8,
@@ -80,34 +75,30 @@ skill2.onRelease((arg) => {
       cause: EntityDamageCause.none
     });
   }, 1 * TicksPerSecond);
+  system.runTimeout(() => {
+    system.clearRun(handle);
+  }, 10 * TicksPerSecond);
 });
 
 amnestyPastor.config.skills = [skill1, skill2];
 
 amnestyPastor.onHitEntity((arg) => {
   const player = arg.damagingEntity as Player;
-  // 赦罪之舞：对敌人进行近战攻击时，有45%的概率回复等级*0.8 的血量
-  // 圣罚赎免：领域持续期间自身近战攻击的回血概率（固有天赋）提升至 100%。
-  new RandomEvent(skill2.isReleasing(player) ? 1 : 0.45, () => {
+  new RandomEvent(0.45, () => {
     heal(player, amnestyPastor.getLevel(player) * 0.8);
   }).call();
   const hurtEntity = arg.hitEntity;
   if (!hurtEntity.isValid) return;
-  if (skill1.isReleasing(player)) {
-    const isWeakness = hurtEntity.getEffect("minecraft:weakness") != undefined;
-    // 赎罪重击：对敌方目标进行近战攻击时，造成5+等级*1.2的额外伤害
-    // 赎罪重击：若目标处于虚弱状态，伤害效果额外提升 50%。
-    const damage = (5 + amnestyPastor.getLevel(player) * 1.2) * (isWeakness ? 1.5 : 1);
+  let chance = 0.5;
+  if (player.hasTag("hiddenyears:amnesty_pastor_skill_2")) chance = 1;
+  new RandomEvent(chance, () => {
+    hurtEntity.addEffect("minecraft:weakness", 15 * TicksPerSecond);
+  }).call();
+  if (player.hasTag("hiddenyears:amnesty_pastor_skill_1")) {
+    const damage = 5 + amnestyPastor.getLevel(player) * 1.2;
     hurtEntity.applyDamage(damage, {
       cause: EntityDamageCause.none
     });
-    // 赎罪重击：同时为自身恢复重击伤害*0.5的生命值。
-    // 赎罪重击：若目标处于虚弱状态，回血效果额外提升 50%。
-    heal(player, damage * 0.5 * (isWeakness ? 1.5 : 1));
+    heal(player, damage * 0.5);
   }
-  // 赦罪之舞：对敌人进行近战攻击时，50% 的概率让敌人获得 15 秒 虚弱。
-  // 放在技能一效果后面
-  new RandomEvent(0.5, () => {
-    hurtEntity.addEffect("minecraft:weakness", 15 * TicksPerSecond);
-  }).call();
 });
--- src/server/job/advanced/arcaneWizard.ts
+++ src/server/job/advanced/arcaneWizard.ts
@@ -53,7 +53,7 @@ const skill2 = new JobSkill(
   15 * TicksPerSecond
 );
 
-// 圣言惩击：对半径 3 格内敌人造成等级*1.5 的神圣伤害，同时回复自身等级*0.8 的生命值。
+// 对半径 3 格内敌人造成 等级*1.5 的神圣伤害，同时回复自身等级*0.8 的生命值。
 skill1.onRelease((arg) => {
   const player = arg.source;
   heal(player, arcaneWizard.getLevel(player) * 0.8);
@@ -62,53 +62,40 @@ skill1.onRelease((arg) => {
     maxDistance: 3,
     families: ["monster"]
   }).applyDamage(arcaneWizard.getLevel(player) * 1.5, {
-    cause: EntityDamageCause.magic,
-    damagingEntity: player
+    cause: EntityDamageCause.none
   });
 });
 
 // 生成持续 15 秒的神圣领域，
 // 领域内法术伤害提升等级*0.6，敌方受到每秒等级*0.5 的持续神圣伤害。
-// 实现差异：地方收到的神圣伤害为无来源伤害
 skill2.onRelease((arg) => {
   const player = arg.source;
   const handle = system.runInterval(() => {
-    if (!skill2.isReleasing(player)) {
-      system.clearRun(handle);
-      return;
-    }
     new EntitiesUtils(player.dimension, {
       location: player.location,
       maxDistance: 10,
       families: ["monster"]
     }).applyDamage(arcaneWizard.getLevel(player) * 0.5, {
-      cause: EntityDamageCause.magic,
-      damagingEntity: undefined
+      cause: EntityDamageCause.none
     });
   }, 1 * TicksPerSecond);
+  system.runTimeout(() => {
+    system.clearRun(handle);
+  }, 15 * TicksPerSecond);
 });
 
 arcaneWizard.config.skills = [skill1, skill2];
 
 arcaneWizard.onCauseDamage((arg) => {
-  // 圣力庇佑：造成法术伤害时，15% 概率获得 1 层「圣盾」（伤害吸收 II 10 秒）
   if (arg.damageSource.cause != EntityDamageCause.magic) return;
   const player = arg.damageSource.damagingEntity as Player;
-  shield(player);
-  // 神圣爆发：领域内法术伤害提升等级*0.6，敌方受到每秒等级*0.5 的持续神圣伤害。
-  // TODO：实现偏差：实际实现为领域持续期间，释放者多对受伤实体造成一次0.6倍无来源魔法伤害
-  if (!arg.hurtEntity.isValid) return;
-  if (skill2.isReleasing(player)) {
+  new RandomEvent(0.15, () => {
+    player.addEffect("minecraft:absorption", 10, { amplifier: 1 });
+  }).call();
+  if (player.hasTag("hiddenyears:arcane_wizard_skill_2")) {
     arg.hurtEntity.applyDamage(arcaneWizard.getLevel(player) * 0.6, {
       cause: EntityDamageCause.magic,
-      // 防止再次触发二技能导致左脚右脚上天
       damagingEntity: null
     });
   }
 });
-
-function shield(player: Player) {
-  new RandomEvent(0.15, () => {
-    player.addEffect("minecraft:absorption", 10, { amplifier: 1 });
-  }).call();
-}
