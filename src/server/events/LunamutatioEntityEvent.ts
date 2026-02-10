57287cdade9f4ed895c438c096c7962a1b924fee
方琉璃 <mucigames@outlook.com>
2026-02-10 11:11:09 +0800

    feat:修改了月蚀怪物的伤害机制

--- src/server/events/LunamutatioEntityEvent.ts
+++ src/server/events/LunamutatioEntityEvent.ts
@@ -1,24 +1,37 @@
-import { world } from "@minecraft/server";
-import { RandomEvent } from "@occultus/api";
+import { EntityDamageCause, world } from "@minecraft/server";
+import { RandomEvent, Time } from "@occultus/api";
 
 export class LunamutatioEntityEvent {
   static subscribe() {
     world.afterEvents.entityHitEntity.subscribe((event) => {
       const { hitEntity, damagingEntity } = event;
-      if (!damagingEntity.matches({ families: ["lunamutatio_monster"] }))
+      if (!damagingEntity.matches({ families: ["basic_lunamutatio_monster"] }))
         return;
       if (!hitEntity.isValid) return;
-      new RandomEvent(0.6, () => {
+      const brightness = Time.getMoonBrightness();
+      console.log("Brightness:" + brightness);
+      hitEntity.applyDamage(10 * brightness, {
+        cause: EntityDamageCause.wither,
+        damagingEntity: null
+      });
+      new RandomEvent(brightness, () => {
         hitEntity.addEffect("wither", 5 * 20);
       }).call();
     });
     world.afterEvents.projectileHitEntity.subscribe((event) => {
       const { source } = event;
-      if (!source.matches({ families: ["lunamutatio_monster"] })) return;
-      if (!event.getEntityHit().entity) return;
-      if (!event.getEntityHit().entity.isValid) return;
-      new RandomEvent(0.6, () => {
-        event.getEntityHit().entity.addEffect("wither", 5 * 20);
+      const hitEntity = event.getEntityHit().entity;
+      if (!source.matches({ families: ["basic_lunamutatio_monster"] })) return;
+      if (!hitEntity) return;
+      if (!hitEntity.isValid) return;
+      const brightness = Time.getMoonBrightness();
+      console.log("Brightness:" + brightness);
+      hitEntity.applyDamage(2 * brightness, {
+        cause: EntityDamageCause.wither,
+        damagingEntity: null
+      });
+      new RandomEvent(brightness, () => {
+        hitEntity.addEffect("wither", 5 * 20);
       }).call();
     });
     world.afterEvents.entityHitEntity.subscribe((event) => {
@@ -26,19 +39,30 @@ export class LunamutatioEntityEvent {
       if (!damagingEntity.matches({ families: ["lunamutatio_traveler"] }))
         return;
       if (!hitEntity.isValid) return;
-      new RandomEvent(0.9, () => {
+      const brightness = Time.getMoonBrightness();
+      console.log("Brightness:" + brightness);
+      hitEntity.applyDamage(12 * brightness, {
+        cause: EntityDamageCause.wither,
+        damagingEntity: null
+      });
+      new RandomEvent(brightness, () => {
         hitEntity.addEffect("wither", 10 * 20, { amplifier: 1 });
       }).call();
     });
     world.afterEvents.projectileHitEntity.subscribe((event) => {
       const { source } = event;
-      if (!source.matches({ families: ["lunamutatio_monster"] })) return;
-      if (!event.getEntityHit().entity) return;
-      if (!event.getEntityHit().entity.isValid) return;
+      const hitEntity = event.getEntityHit().entity;
+      if (!source.matches({ families: ["lunamutatio_traveler"] })) return;
+      if (!hitEntity) return;
+      if (!hitEntity.isValid) return;
+      const brightness = Time.getMoonBrightness();
+      console.log("Brightness:" + brightness);
+      hitEntity.applyDamage(4 * brightness, {
+        cause: EntityDamageCause.wither,
+        damagingEntity: null
+      });
       new RandomEvent(0.9, () => {
-        event
-          .getEntityHit()
-          .entity.addEffect("wither", 10 * 20, { amplifier: 1 });
+        hitEntity.addEffect("wither", 10 * 20, { amplifier: 1 });
       }).call();
     });
   }
