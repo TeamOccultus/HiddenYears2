2f0cd94f1fcbbf2442030c099ee4a0d9574558d5
方琉璃 <mucigames@outlook.com>
2026-02-17 14:00:01 +0800

    feat:移除了测试版提示

--- src/server/msg.ts
+++ src/server/msg.ts
@@ -5,10 +5,10 @@ export function registryMessage() {
   new WelcomeNotification((player: Player) => {
     return {
       rawtext: [
-        { translate: "message.hiddenyears:welcome" },
-        { text: Format.newLine },
+        { translate: "message.hiddenyears:welcome" }
+        /*{ text: Format.newLine },
         { text: Color.red },
-        { translate: "message.hiddenyears:is_beta" }
+        { translate: "message.hiddenyears:is_beta" }*/
       ]
     };
   });
