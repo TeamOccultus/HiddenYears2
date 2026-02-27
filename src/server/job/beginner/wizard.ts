f8f9925e5d7a1514bde6ccca7601210ee0b950b8
方琉璃 <Fangliulii@outlook.com>
2026-02-28 06:52:03 +0800

    feat:提高了升级所需的统一货币值数目

--- src/server/job/beginner/wizard.ts
+++ src/server/job/beginner/wizard.ts
@@ -27,12 +27,12 @@ export const wizard = new Job(
       {
         min: 0,
         max: 5,
-        condition: [new UnifiedCurrencyValueConditions(200, true)]
+        condition: [new UnifiedCurrencyValueConditions(1200, true)]
       },
       {
         min: 6,
         max: 10,
-        condition: [new UnifiedCurrencyValueConditions(500, true)]
+        condition: [new UnifiedCurrencyValueConditions(2000, true)]
       }
     ],
     transform: [
