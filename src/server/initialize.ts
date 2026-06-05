6a850cf1f15ad8eedc6e3b5e9f3dd9293e647682
方琉璃 <mucigames@outlook.com>
2026-06-05 21:06:05 +0800

    v3.0.9-beta.3

--- src/server/initialize.ts
+++ src/server/initialize.ts
@@ -37,7 +37,7 @@ export function initialize() {
   new ModInitializer(
     "hiddenyears",
     "Hidden Years²: Governor at the Skyline",
-    "3.0.9-beta.2"
+    "3.0.9-beta.3"
   );
   registryCommands();
   registryCustomRecipe();
