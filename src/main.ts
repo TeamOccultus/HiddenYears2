44e258987576c23e69253bb29a8baa5928d9c709
方琉璃 <mucigames@outlook.com>
2026-07-11 13:09:19 +0800

    test:测试对话功能

--- src/main.ts
+++ src/main.ts
@@ -4,31 +4,31 @@
  * 这个模块没有任何类、函数或变量，它只是负责调用编写好的初始化函数
  *
  * > 在这一刻，我的心平静如水
- * > 
+ * >
  * > 没有谁能迫我退悔
- * > 
+ * >
  * > 四面楚歌，这颗心无惧无畏
- * > 
+ * >
  * > 铁花飞，飘逸不残灰
- * > 
+ * >
  * > 千年之后还剩下什么？
  * >
  * > 灵魂的重量又怎能取舍？
- * > 
+ * >
  * > 有共存才有未来可说
- * > 
+ * >
  * > 不一定牵着手
- * > 
+ * >
  * > 但一定永远向前走
- * > 
+ * >
  * > 正确了又能证明什么？
- * > 
+ * >
  * > 就算能从过去找出差错
- * > 
+ * >
  * > 身世境遇不由人选择
- * > 
+ * >
  * > 有理解，就足够
- * > 
+ * >
  * > 盼日夜，细水长流
  * >
  * > ——Mili《铁花飞》
@@ -36,8 +36,19 @@
  * @module HiddenYears2
  * @category Main
  */
-import { world } from "@minecraft/server";
+import { GameMode, system, world } from "@minecraft/server";
 import { initialize } from "./server/initialize";
 
 initialize();
 
+world.beforeEvents.playerInteractWithEntity.subscribe((event) => {
+  if (event.target.typeId === "hiddenyears:trial_zombie_1") {
+    if (event.player.getGameMode() === GameMode.Creative) {
+      event.cancel = true;
+      system.run(() => {
+        event.player.setGameMode(GameMode.Survival);
+        event.target.runCommand(`dialogue open @s ${event.player.name} hiddenyears:example`);
+      });
+    }
+  }
+});
